512334ab391f884619284de39e3bc917ef8a715b
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-08-03 01:19:10 +0500

    secure application

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const express = require('express')
-const colors = require('colors')
-//  rest object
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+//const express = require('express')
+//const colors = require('colors')
 
-const app = express()
+//configure env
+dotenv.config();
+
+//  rest object
+const app = express();
 
 // rest api
 
 app.get('/', (req, res) => {
-    res.send({
+    res.send('<h1>Welcome to ecommerce app</h1>');
+    /**res.send({
         message: 'welcome to ecommerce app'
-    })
+    })**/
+
+
 })
 
 // port
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 // run app
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`.bgCyan.white);
+    console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 })

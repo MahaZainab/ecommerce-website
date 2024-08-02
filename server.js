81030ae930b6abd189fbc6b4a5056cd9006df0c9
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-08-03 00:58:34 +0500

    rest API through express

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const colors = require('colors')
+//  rest object
+
+const app = express()
+
+// rest api
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'welcome to ecommerce app'
+    })
+})
+
+// port
+const PORT = 8080
+// run app
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`.bgCyan.white);
+})
